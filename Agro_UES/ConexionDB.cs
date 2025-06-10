4542c528870bed2cbc482faf6d5a3b25764f5dd7
Axeliox-Salvatore <HM23052@ues.edu.sv>
2025-06-10 17:53:29 -0600

    BAse de datos

---
+++ Agro_UES/ConexionDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+
+namespace Agro_UES
+{
+    class ConexionDB
+    {
+        private string conexionString = "server=localhost; database=AgroServicioDB; user=root; password=tu_contraseña;";
+
+        public MySqlConnection Conectar()
+        {
+            try
+            {
+                MySqlConnection conexion = new MySqlConnection(conexionString);
+                conexion.Open();
+                return conexion;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error de conexión: " + ex.Message);
+                return null;
+            }
+        }
+
+        public void CerrarConexion(MySqlConnection conexion)
+        {
+            if (conexion != null)
+            {
+                conexion.Close();
+            }
+        }
+
+    }
+}
